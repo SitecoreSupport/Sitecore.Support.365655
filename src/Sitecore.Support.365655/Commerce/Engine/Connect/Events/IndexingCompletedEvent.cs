2d8ba4d2a8e04e6cb13b67f2bfee3366cacd765e
mao <mao@sitecore.net>
2019-10-16 15:16:56 +0300

    Add indexing:completed event handler.

---
+++ src/Sitecore.Support.365655/Commerce/Engine/Connect/Events/IndexingCompletedEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Support.Commerce.Engine.Connect.Events
+{
+    [DataContract]
+    public class IndexingCompletedEvent
+    {
+        [DataMember]
+        public string DatabaseName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string[] SitecoreIds
+        {
+            get;
+            set;
+        }
+    }
+}
