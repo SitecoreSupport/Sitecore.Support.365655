763604799db1c3fa1f22703d76e4846003b2217e
mao <mao@sitecore.net>
2019-10-16 15:17:34 +0300

    Add indexing:completed argument.

---
+++ src/Sitecore.Support.365655/Commerce/Engine/Connect/Events/IndexingCompletedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Support.Commerce.Engine.Connect.Events
+{
+    public class IndexingCompletedEventArgs : EventArgs, IPassNativeEventArgs
+    {
+        private readonly IndexingCompletedEvent _evt;
+
+        public string DatabaseName => _evt.DatabaseName ?? string.Empty;
+
+        public string[] SitecoreIds => _evt.SitecoreIds ?? new string[0];
+
+        public IndexingCompletedEventArgs(IndexingCompletedEvent evt)
+        {
+            _evt = evt;
+        }
+    }
+}
