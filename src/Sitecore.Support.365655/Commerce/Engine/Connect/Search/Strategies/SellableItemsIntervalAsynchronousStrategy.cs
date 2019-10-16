182215caf6dd02ad95d65691b67826c19838a8e9
mao <mao@sitecore.net>
2019-10-16 15:22:17 +0300

    Implement indexing strategy for sellable items, categories and catalogs.

---
+++ src/Sitecore.Support.365655/Commerce/Engine/Connect/Search/Strategies/SellableItemsIntervalAsynchronousStrategy.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Commerce.Engine.Connect.Search;
+using Sitecore.Commerce.Plugin.Catalog;
+using Sitecore.Commerce.Plugin.ManagedLists;
+using Sitecore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Commerce.Engine.Connect.Search.Strategies
+{
+    public class SellableItemsIntervalAsynchronousStrategy : CatalogSystemIntervalAsynchronousStrategyBase<SellableItem>
+    {
+        public SellableItemsIntervalAsynchronousStrategy(string interval, string database) : base(interval, database)
+        {
+        }
+
+        protected override ManagedList GetIncrementalEntitiesToIndex(string environment, int skip)
+        {
+            Assert.ArgumentNotNullOrEmpty(environment, "environment");
+            return IndexUtility.GetSellableItemsToIndex(environment, base.IncrementalIndexListName, skip, base.ItemsToTake);
+        }
+    }
+}
