86679bf57c9b4b3b78d5bdddca677789dcef8d41
mao <mao@sitecore.net>
2019-10-16 15:17:50 +0300

    Add indexing:completed hook.

---
+++ src/Sitecore.Support.365655/Commerce/Engine/Connect/Events/IndexingCompletedHook.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Eventing;
+using Sitecore.Events.Hooks;
+using Sitecore.Support.Commerce.Engine.Connect.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Commerce.Engine.Connect.Events
+{
+    public class IndexingCompletedHook : IHook
+    {
+        public void Initialize()
+        {
+            EventManager.Subscribe<IndexingCompletedEvent>(IndexingCompletedEventHandler.Run);
+        }
+    }
+}
